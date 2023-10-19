70f46c6018c687f493e0bc62ea004467cc5b566e
carlosfernandino <142689457+carlosfernandino@users.noreply.github.com>
2023-10-19 19:43:25 -0300

    Delete tratamento-excessoes directory

--- tratamento-excessoes/src/exceptions/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class BusinessException extends RuntimeException {
-    private static final long serialVersion = 1L;
-    public BusinessException(String msg){
-        super(msg);
-    }
-}
